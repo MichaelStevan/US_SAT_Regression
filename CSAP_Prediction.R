0388814f726074f987062017350fcb0cd2af49bb
Miguel Esteban <miestgo@gmail.com>
2018-05-16 17:49:44 +0200

    Cleaned code.

--- CSAP_Prediction.R
+++ CSAP_Prediction.R
@@ -9,16 +9,18 @@ states.data <- readRDS("states.rds")
 # Look at the data
 head(states.data)
 
-# Predictions Goal: csat score
-# Remove vsat and msat since the sum of both gives the prediction we want.
+# Prediction Goal: CSAT score
+# Remove vsat and msat columns, since the sum of both gives the prediction we want to obtain
+# from all other variables.
 states.data$vsat <- NULL
 states.data$msat <- NULL
 
 # Prepare training scheme
 control <- trainControl(method="cv", number=5)
 
-# Omit state variable
+# Omit state variable, it proves not useful
 states.data.filter <- states.data %>% select(-state)
+
 # Remove rows with NAs
 states.data.filter <- states.data.filter[complete.cases(states.data.filter),]
 summary(states.data.filter)
@@ -38,16 +40,12 @@ set.seed(27)
 model_Svm <- train(csat~., data=states.data.filter, method="svmRadial", trControl=control)
 summary(model_Svm)
 
-# collect resamples
+# Collect resamples
 results <- resamples(list(LM=model_Lm,SVM=model_Svm,GLMNET = model_Glmnet))
-# summarize the distributions
+
+# summarize the distributions along the cvs
 summary(results)
-# boxplots of results
+# boxplot
 bwplot(results)
-# dot plots of results
+# dot plot
 dotplot(results)
-
-
-
-
-
